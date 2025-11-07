8f7ce6db87f6e8bb6347b318945f3375d56ef35f
No Name <Vanii27@mail.ru>
2025-11-07 17:26:10 +0300

    Complete add solve for Ex

--- LAB1.1/main.sh
+++ LAB1.1/main.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for word in "$@"; do
-	reversed=$(echo "$word" | rev)
+is_palindrome() {
+	local word="$1"
+	local reversed=$(echo "$1" | rev)
 	if [ "$word" = "$reversed" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+for word in "$@"; do
+	if is_palindrome "$word"; then
 		echo "'$word' - palindrome"
 	else
 		echo "'$word' - not palindrome"
